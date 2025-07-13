b02525acff6b209a1d8c360bd567b4176b5df281
dewi-xaltius <smdewi@xaltius.tech>
2025-07-13 17:00:57 +0800

    Initial Vite React App commit

--- src/store/actions.js
+++
@@ -1,9 +0,0 @@
-// store/actions.js
-export const UPDATE_MESSAGE = 'UPDATE_MESSAGE';
-
-export const updateMessage = message => {
-    return {
-        type: UPDATE_MESSAGE,
-        payload: message
-    };
-};
